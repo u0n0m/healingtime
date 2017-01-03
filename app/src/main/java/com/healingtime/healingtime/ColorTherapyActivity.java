f4d5d7a4c6f2a26ba3822846a1c0de2b51b1c8cf
skh1 <u0n0m@naver.com>
2017-01-03 09:15:54 +0900

    껍데기 앱 제작중

---
+++ app/src/main/java/com/healingtime/healingtime/ColorTherapyActivity.java
@@ -0,0 +1,13 @@
+package com.healingtime.healingtime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ColorTherapyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colortherapy_main);
+    }
+}
