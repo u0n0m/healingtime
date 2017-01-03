b31b12d71017d7f50c3b4a4f7968f9f87f62c9ef
skh1 <u0n0m@naver.com>
2017-01-03 16:35:44 +0900

    껍데기 앱 제작중 - 아로마 분사 테스트

--- app/src/main/java/com/healingtime/healingtime/TherapyDictionaryActivity.java
+++ app/src/main/java/com/healingtime/healingtime/TherapyDictionaryActivity.java
@@ -9,5 +9,18 @@ public class TherapyDictionaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.therapy_dictionary);
+
+/*        ListView ListView=(ListView)findViewById(R.id.listview);
+        ArrayList<Listviewitem> data=new ArrayList<>();
+        Listviewitem lion=new Listviewitem(R.drawable.aroma_therapy,"Lion");
+        Listviewitem tiger=new Listviewitem(R.drawable.aroma_therapy,"Tiger");
+        Listviewitem dog=new Listviewitem(R.drawable.aroma_therapy,"Dog");
+        Listviewitem cat=new Listviewitem(R.drawable.aroma_therapy,"Cat");
+        data.add(lion);
+        data.add(tiger);
+        data.add(dog);
+        data.add(cat);
+        ListviewAdapter adapter=new ListviewAdapter(this,R.layout.therapy_dictionary,data);
+        ListView.setAdapter(adapter);*/
     }
 }
