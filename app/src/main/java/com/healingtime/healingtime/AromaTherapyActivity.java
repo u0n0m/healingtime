92537f798cc8c5fd77fb54bfacc8955fa49d9e9c
skh1 <u0n0m@naver.com>
2017-01-09 11:20:16 +0900

    아로마 분사 테스트 완료 - 발향기-> 앱 (아로마 구간A ) 설정값 전송받아 표시하는 기능 구현 완료

--- app/src/main/java/com/healingtime/healingtime/AromaTherapyActivity.java
+++ app/src/main/java/com/healingtime/healingtime/AromaTherapyActivity.java
@@ -1,11 +1,14 @@
 package com.healingtime.healingtime;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AromaTherapyActivity extends AppCompatActivity {
@@ -78,9 +81,30 @@ public class AromaTherapyActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        SharedPreferences sp_aroma_settings = getApplicationContext().getSharedPreferences("AromaSettings", Context.MODE_PRIVATE); // 설정 정보 읽어오기
+        if(sp_aroma_settings.getBoolean("aroma_a_type", false) ) {
+            Integer start_hour = sp_aroma_settings.getInt("aroma_a_data1", 1);
+            Integer start_minute = sp_aroma_settings.getInt("aroma_a_data2", 2);
+            Integer stop_hour = sp_aroma_settings.getInt("aroma_a_data3", 3);
+            Integer stop_minute = sp_aroma_settings.getInt("aroma_a_data4", 4);
+            Integer cycle_week = sp_aroma_settings.getInt("aroma_a_data5", 5);
+            Integer cycle_time = sp_aroma_settings.getInt("aroma_a_data6", 6);
+            Integer aroma_type = sp_aroma_settings.getInt("aroma_a_data7", 7);
+            Integer aroma_remain = sp_aroma_settings.getInt("aroma_a_data8", 8);
 
+            RelativeLayout aroma_therapy_setting_value_a = (RelativeLayout) findViewById(R.id.aroma_therapy_setting_value_a);  //설정값 표시 화면 다시 보이게 만들기
+            aroma_therapy_setting_value_a.setVisibility(View.VISIBLE);
 
+            TextView aroma_therapy_setting_a_time = (TextView) findViewById(R.id.aroma_therapy_setting_a_time);  //설
+            String set1 = start_hour.toString() + ":" + start_minute.toString() + "~" + stop_hour.toString() + ":" + stop_minute.toString();
+            aroma_therapy_setting_a_time.setText(set1);
+        }
+    }
 }
