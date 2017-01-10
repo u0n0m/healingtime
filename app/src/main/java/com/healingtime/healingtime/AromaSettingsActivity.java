45c7532e6aaacb698141dc84ff2aac24e321c3ca
skh1 <u0n0m@naver.com>
2017-01-10 13:20:41 +0900

    블루투스 연동 성공 - 실제 기기와 테스트용으로 진행중

--- app/src/main/java/com/healingtime/healingtime/AromaSettingsActivity.java
+++ app/src/main/java/com/healingtime/healingtime/AromaSettingsActivity.java
@@ -1,11 +1,25 @@
 package com.healingtime.healingtime;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class AromaSettingsActivity extends AppCompatActivity {
+    private byte aroma_type;
+    private Integer start_hour =6, start_minute=30, end_hour=19, end_minute=30;
+    private Integer week_sum = 0;
+    private String week_sum_string = "반복: ";
+    private Integer cycle_time = 30;
+    private Integer remain_amount;
 
     @Override
     public void onBackPressed() {
@@ -17,10 +31,191 @@ public class AromaSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aroma_settings);
 
-        ImageView color_therapy = (ImageView) findViewById(R.id.aroma_settings_back_btn);
-        color_therapy.setOnClickListener(new View.OnClickListener() {
+        TimePicker timepicker_starttime = (TimePicker) findViewById(R.id.timepicker_starttime);
+        timepicker_starttime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            public void onTimeChanged(TimePicker view, int  hourOfDay, int minute){
+                start_hour = hourOfDay;
+                start_minute = minute;
+            }
+        });
+
+        TimePicker timepicker_endtime = (TimePicker) findViewById(R.id.timepicker_endtime);
+        timepicker_endtime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            public void onTimeChanged(TimePicker view, int  hourOfDay, int minute){
+                end_hour = hourOfDay;
+                end_minute = minute;
+            }
+        });
+
+        ToggleButton aroma_toggle_sun = (ToggleButton) findViewById(R.id.aroma_toggle_sun);
+        ToggleButton aroma_toggle_mon = (ToggleButton) findViewById(R.id.aroma_toggle_mon);
+        ToggleButton aroma_toggle_tue = (ToggleButton) findViewById(R.id.aroma_toggle_tue);
+        ToggleButton aroma_toggle_wed = (ToggleButton) findViewById(R.id.aroma_toggle_wed);
+        ToggleButton aroma_toggle_thu = (ToggleButton) findViewById(R.id.aroma_toggle_thu);
+        ToggleButton aroma_toggle_fri = (ToggleButton) findViewById(R.id.aroma_toggle_fri);
+        ToggleButton aroma_toggle_sat = (ToggleButton) findViewById(R.id.aroma_toggle_sat);
+        aroma_toggle_sun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                week_sum += 0x40;
+                week_sum_string += "일,";
+            }
+        });
+        aroma_toggle_mon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x20;
+                week_sum_string += "월,";
+            }
+        });
+        aroma_toggle_tue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x10;
+                week_sum_string += "화,";
+            }
+        });
+        aroma_toggle_wed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x08;
+                week_sum_string += "수,";
+            }
+        });
+        aroma_toggle_thu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x04;
+                week_sum_string += "목,";
+            }
+        });
+        aroma_toggle_fri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x02;
+                week_sum_string += "금,";
+            }
+        });
+        aroma_toggle_sat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                week_sum += 0x01;
+                week_sum_string +="토,";
+            }
+        });
+
+        SeekBar seekbar_cycle_time = (SeekBar) findViewById(R.id.seekbar_cycle_time);
+        seekbar_cycle_time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            public void onProgressChanged(SeekBar seekBar, int time, boolean fromUser){
+                cycle_time = time;
+                EditText aroma_therapy_cycle_time = (EditText) findViewById(R.id.aroma_therapy_cycle_time);
+                aroma_therapy_cycle_time.setText(cycle_time.toString() + "분");
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        ImageView aroma_settings_back_btn = (ImageView) findViewById(R.id.aroma_settings_back_btn);
+        aroma_settings_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Button aroma_therapy_time_settings_cancel = (Button) findViewById(R.id.aroma_therapy_time_settings_cancel);
+        aroma_therapy_time_settings_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Button aroma_therapy_time_settings_save = (Button) findViewById(R.id.aroma_therapy_time_settings_save);
+        aroma_therapy_time_settings_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton aroma_therapy_radiobutton_a = (RadioButton) findViewById(R.id.aroma_therapy_radiobutton_a);
+                RadioButton aroma_therapy_radiobutton_b  = (RadioButton) findViewById(R.id.aroma_therapy_radiobutton_b);
+                RadioButton aroma_therapy_radiobutton_c = (RadioButton) findViewById(R.id.aroma_therapy_radiobutton_c);
+
+                SharedPreferences sp_aroma_settings = getApplicationContext().getSharedPreferences("AromaSettings", Context.MODE_PRIVATE); // 설정 정보 읽어오기
+                SharedPreferences.Editor editor = sp_aroma_settings.edit(); //sharedPreference 내용 수정
+
+                if(aroma_therapy_radiobutton_a.isChecked()){
+                    aroma_type = 0x12;
+                    editor.putBoolean("aroma_a_type",true); //아로마 A구간 설정 여부
+                    editor.putInt("aroma_a_data1",start_hour); //아로마 A구간 Data1 필드 저장(시작시)
+                    editor.putInt("aroma_a_data2",start_minute); //아로마 A구간 Data2 필드 저장(시작분)
+                    editor.putInt("aroma_a_data3",end_hour); //아로마 A구간 설정 패킷의 Data3 필드 저장(종료시)
+                    editor.putInt("aroma_a_data4",end_minute); //아로마 A구간 설정 패킷의 Data4 필드 저장(종료분)
+                    editor.putInt("aroma_a_data5",week_sum); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putString("aroma_a_data5_1",week_sum_string); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putInt("aroma_a_data6",cycle_time); //아로마 A구간 설정 패킷의 Data6 필드 저장(분사간격)
+                    editor.putInt("aroma_a_data7",0x00); //아로마 A구간 설정 패킷의 Data7 필드 저장(아로마종류)
+                    editor.putInt("aroma_a_data8",0xFF); //아로마 A구간 설정 패킷의 Data8 필드 저장(잔여량)
+                    editor.putString("aroma_a_name", "basil");
+                    editor.commit();
+                }
+                else if(aroma_therapy_radiobutton_b.isChecked()){
+                    aroma_type = 0x13;
+                    editor.putBoolean("aroma_b_type",true); //아로마 A구간 설정 패킷여부
+                    editor.putInt("aroma_b_data1",start_hour); //아로마 A구간 Data1 필드 저장(시작시)
+                    editor.putInt("aroma_b_data2",start_minute); //아로마 A구간 Data2 필드 저장(시작분)
+                    editor.putInt("aroma_b_data3",end_hour); //아로마 A구간 설정 패킷의 Data3 필드 저장(종료시)
+                    editor.putInt("aroma_b_data4",end_minute); //아로마 A구간 설정 패킷의 Data4 필드 저장(종료분)
+                    editor.putInt("aroma_b_data5",week_sum); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putString("aroma_b_data5_1",week_sum_string); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putInt("aroma_b_data6",cycle_time); //아로마 A구간 설정 패킷의 Data6 필드 저장(분사간격)
+                    editor.putInt("aroma_b_data7",0x00); //아로마 A구간 설정 패킷의 Data7 필드 저장(아로마종류)
+                    editor.putInt("aroma_b_data8",0xFF); //아로마 A구간 설정 패킷의 Data8 필드 저장(잔여량)
+                    editor.putString("aroma_b_name", "bergamot_calabrian");
+                    editor.commit();
+                }
+                else if(aroma_therapy_radiobutton_c.isChecked()){
+                    aroma_type = 0x14;
+                    editor.putBoolean("aroma_c_type",true); //아로마 A구간 설정 패킷여부
+                    editor.putInt("aroma_c_data1",start_hour); //아로마 A구간 Data1 필드 저장(시작시)
+                    editor.putInt("aroma_c_data2",start_minute); //아로마 A구간 Data2 필드 저장(시작분)
+                    editor.putInt("aroma_c_data3",end_hour); //아로마 A구간 설정 패킷의 Data3 필드 저장(종료시)
+                    editor.putInt("aroma_c_data4",end_minute); //아로마 A구간 설정 패킷의 Data4 필드 저장(종료분)
+                    editor.putInt("aroma_c_data5",week_sum); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putString("aroma_c_data5_1",week_sum_string); //아로마 A구간 설정 패킷의 Data5 필드 저장(요일)
+                    editor.putInt("aroma_c_data6",cycle_time); //아로마 A구간 설정 패킷의 Data6 필드 저장(분사간격)
+                    editor.putInt("aroma_c_data7",0x00); //아로마 A구간 설정 패킷의 Data7 필드 저장(아로마종류)
+                    editor.putInt("aroma_c_data8",0xFF); //아로마 A구간 설정 패킷의 Data8 필드 저장(잔여량)
+                    editor.putString("aroma_c_name", "cedarwood_atlas");
+                    editor.commit();
+                }
+
+                //아로마 설정값 발향기로 전송
+                int finchk = 254;
+                int s_hour = start_hour, s_min=start_minute, e_hour = end_hour, e_min = end_minute, w_sum = week_sum, c_time = cycle_time;
+
+                byte start_frame = (byte) 0x10; //프레임 시작
+                byte type_frame = (byte) aroma_type;
+                byte start_hour = (byte) s_hour;
+                byte start_min = (byte) s_min;
+                byte end_hour = (byte) e_hour;
+                byte end_min = (byte) e_min;
+                byte week_sum = (byte) w_sum;
+                byte cycle_min = (byte) c_time;  //30분 테스트용
+                byte aroma_type = (byte) 0x00;  //향기종류 //현재 사용하지 않음 // 그냥 0으로 설정
+                byte aroma_remain = (byte) 0xFF; // 잔여량
+                byte padding = (byte) 0x00;  //단순 패딩임..
+                byte fin_frame = (byte) finchk; // 프레임 종료
+                byte [] aroma_set = {start_frame, type_frame, start_hour, start_min, end_hour, end_min, week_sum, cycle_min, aroma_type, aroma_remain, padding, padding, fin_frame};
+
+                try {
+                    ((MainActivity) MainActivity.mContext).mOutputStream.write(aroma_set);
+                }catch(Exception  e){
+                    e.printStackTrace();
+                }
                 onBackPressed();
             }
         });
--- app/src/main/java/com/healingtime/healingtime/AromaTherapyActivity.java
+++ app/src/main/java/com/healingtime/healingtime/AromaTherapyActivity.java
@@ -1,10 +1,12 @@
 package com.healingtime.healingtime;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -22,7 +24,9 @@ public class AromaTherapyActivity extends AppCompatActivity {
         aroma_therapy_aroma_set_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), AromaSelectActivity.class);
+                startActivity(intent);
+                //Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
         ImageView aroma_therapy_aroma_remaining_btn = (ImageView) findViewById(R.id.aroma_therapy_aroma_remaining_btn);
@@ -37,7 +41,6 @@ public class AromaTherapyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "분사버튼 터치됨!", Toast.LENGTH_SHORT).show();
-                //((MainActivity)MainActivity.mContext).sendData("Aroma");
                 int finchk = 254;
                 byte fin = (byte)finchk;
                 //byte [] color_set = {0x10, 0x11, 0x0e, 0x0a, 0x0e, 0x1e, 0x7e, 0x05, 0x02, 0x22, 0x67, 0x0a, fin};
@@ -60,28 +63,27 @@ public class AromaTherapyActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
-        RelativeLayout color_box1 = (RelativeLayout) findViewById(R.id.color_box1);
-        color_box1.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout aroma_box1 = (RelativeLayout) findViewById(R.id.aroma_box1);
+        aroma_box1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
-        RelativeLayout color_box2 = (RelativeLayout) findViewById(R.id.color_box2);
-        color_box2.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout aroma_box2 = (RelativeLayout) findViewById(R.id.aroma_box2);
+        aroma_box2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
-        RelativeLayout color_box3 = (RelativeLayout) findViewById(R.id.color_box3);
-        color_box3.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout aroma_box3 = (RelativeLayout) findViewById(R.id.aroma_box3);
+        aroma_box3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "기기와 연결되지 않았습니다~\n연결 후 다시 시도하세요!", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
     @Override
@@ -90,21 +92,159 @@ public class AromaTherapyActivity extends AppCompatActivity {
 
         SharedPreferences sp_aroma_settings = getApplicationContext().getSharedPreferences("AromaSettings", Context.MODE_PRIVATE); // 설정 정보 읽어오기
         if(sp_aroma_settings.getBoolean("aroma_a_type", false) ) {
-            Integer start_hour = sp_aroma_settings.getInt("aroma_a_data1", 1);
-            Integer start_minute = sp_aroma_settings.getInt("aroma_a_data2", 2);
-            Integer stop_hour = sp_aroma_settings.getInt("aroma_a_data3", 3);
-            Integer stop_minute = sp_aroma_settings.getInt("aroma_a_data4", 4);
-            Integer cycle_week = sp_aroma_settings.getInt("aroma_a_data5", 5);
-            Integer cycle_time = sp_aroma_settings.getInt("aroma_a_data6", 6);
-            Integer aroma_type = sp_aroma_settings.getInt("aroma_a_data7", 7);
-            Integer aroma_remain = sp_aroma_settings.getInt("aroma_a_data8", 8);
+            Integer start_hour = sp_aroma_settings.getInt("aroma_a_data1", 1);  //시작시
+            Integer start_minute = sp_aroma_settings.getInt("aroma_a_data2", 2); //시작분
+            Integer end_hour = sp_aroma_settings.getInt("aroma_a_data3", 3); //종료시
+            Integer end_minute = sp_aroma_settings.getInt("aroma_a_data4", 4); //종료분
+            Integer cycle_week = sp_aroma_settings.getInt("aroma_a_data5", 5); //분사 주기(요일)
+            String cycle_week_string = sp_aroma_settings.getString("aroma_a_data5_1", "월");
+            Integer cycle_time = sp_aroma_settings.getInt("aroma_a_data6", 6); //분사주기 (분)
+            Integer aroma_type = sp_aroma_settings.getInt("aroma_a_data7", 7); // 향기종료. 사용안함
+            Integer aroma_remain = sp_aroma_settings.getInt("aroma_a_data8", 8); // 잔여량
+            String aroma_a_name = sp_aroma_settings.getString("aroma_a_name", "설정값 없음");
+
+            RelativeLayout aroma_box1 = (RelativeLayout) findViewById(R.id.aroma_box1);
+            String resName1 = "@drawable/aroma_" + aroma_a_name;
+            int resID1 = getResources().getIdentifier(resName1, "drawable", "com.healingtime.healingtime");
+            aroma_box1.setBackgroundResource(resID1);
+
+            ImageView aroma_status_image1 = (ImageView) findViewById(R.id.aroma_status_image1);
+            String resName2 = "@drawable/aroma_" + aroma_a_name;
+            int resID2 = getResources().getIdentifier(resName2, "drawable", "com.healingtime.healingtime");
+            aroma_status_image1.setImageResource(resID2);
 
             RelativeLayout aroma_therapy_setting_value_a = (RelativeLayout) findViewById(R.id.aroma_therapy_setting_value_a);  //설정값 표시 화면 다시 보이게 만들기
             aroma_therapy_setting_value_a.setVisibility(View.VISIBLE);
 
             TextView aroma_therapy_setting_a_time = (TextView) findViewById(R.id.aroma_therapy_setting_a_time);  //설
-            String set1 = start_hour.toString() + ":" + start_minute.toString() + "~" + stop_hour.toString() + ":" + stop_minute.toString();
+            String start_ampm ="오전";
+            String end_ampm ="오전";
+            if(start_hour >= 13) { start_ampm = "오후"; start_hour -= 12;}
+            if(end_hour >= 13) { end_ampm = "오후"; end_hour -= 12;}
+            String set1 = start_ampm + " " + start_hour.toString() + ":" + start_minute.toString() + " ~ " + end_ampm + " " + end_hour.toString() + ":" + end_minute.toString();
             aroma_therapy_setting_a_time.setText(set1);
+
+            TextView aroma_therapy_setting_a_aroma = (TextView) findViewById(R.id.aroma_therapy_setting_a_aroma);  //설
+            String set2 = "향기이름: Basil" ;
+            aroma_therapy_setting_a_aroma.setText(set2);
+
+            TextView aroma_therapy_setting_a_repeat = (TextView) findViewById(R.id.aroma_therapy_setting_a_repeat);  //설
+            String set3 = cycle_week_string + "   주기: " + cycle_time.toString() + "분";
+            aroma_therapy_setting_a_repeat.setText(set3);
         }
+        if(sp_aroma_settings.getBoolean("aroma_b_type", false) ) {
+            Integer start_hour = sp_aroma_settings.getInt("aroma_b_data1", 1);  //시작시
+            Integer start_minute = sp_aroma_settings.getInt("aroma_b_data2", 2); //시작분
+            Integer end_hour = sp_aroma_settings.getInt("aroma_b_data3", 3); //종료시
+            Integer end_minute = sp_aroma_settings.getInt("aroma_b_data4", 4); //종료분
+            Integer cycle_week = sp_aroma_settings.getInt("aroma_b_data5", 5); //분사 주기(요일)
+            String cycle_week_string = sp_aroma_settings.getString("aroma_b_data5_1", "월");
+            Integer cycle_time = sp_aroma_settings.getInt("aroma_b_data6", 6); //분사주기 (분)
+            Integer aroma_type = sp_aroma_settings.getInt("aroma_b_data7", 7); // 향기종료. 사용안함
+            Integer aroma_remain = sp_aroma_settings.getInt("aroma_b_data8", 8); // 잔여량
+            String aroma_b_name = sp_aroma_settings.getString("aroma_b_name", "설정값 없음");
+
+            RelativeLayout aroma_box2 = (RelativeLayout) findViewById(R.id.aroma_box2);
+            String resName1 = "@drawable/aroma_" + aroma_b_name;
+            int resID1 = getResources().getIdentifier(resName1, "drawable", "com.healingtime.healingtime");
+            aroma_box2.setBackgroundResource(resID1);
+
+            ImageView aroma_status_image1 = (ImageView) findViewById(R.id.aroma_status_image1);
+            String resName2 = "@drawable/aroma_" + aroma_b_name;
+            int resID2 = getResources().getIdentifier(resName2, "drawable", "com.healingtime.healingtime");
+            aroma_status_image1.setImageResource(resID2);
+
+            RelativeLayout aroma_therapy_setting_value_b = (RelativeLayout) findViewById(R.id.aroma_therapy_setting_value_b);  //설정값 표시 화면 다시 보이게 만들기
+            aroma_therapy_setting_value_b.setVisibility(View.VISIBLE);
+
+            TextView aroma_therapy_setting_b_time = (TextView) findViewById(R.id.aroma_therapy_setting_b_time);  //설
+            String start_ampm ="오전";
+            String end_ampm ="오전";
+            if(start_hour >= 13) { start_ampm = "오후"; start_hour -= 12;}
+            if(end_hour >= 13) { end_ampm = "오후"; end_hour -= 12;}
+            String set1 = start_ampm + " " + start_hour.toString() + ":" + start_minute.toString() + " ~ " + end_ampm + " " + end_hour.toString() + ":" + end_minute.toString();
+            aroma_therapy_setting_b_time.setText(set1);
+
+            TextView aroma_therapy_setting_b_aroma = (TextView) findViewById(R.id.aroma_therapy_setting_b_aroma);  //설
+            String set2 = "향기이름: Basil" ;
+            aroma_therapy_setting_b_aroma.setText(set2);
+
+            TextView aroma_therapy_setting_b_repeat = (TextView) findViewById(R.id.aroma_therapy_setting_b_repeat);  //설
+            String set3 = cycle_week_string + "   주기: " + cycle_time.toString() + "분";
+            aroma_therapy_setting_b_repeat.setText(set3);
+        }
+        if(sp_aroma_settings.getBoolean("aroma_c_type", false) ) {
+            Integer start_hour = sp_aroma_settings.getInt("aroma_c_data1", 1);  //시작시
+            Integer start_minute = sp_aroma_settings.getInt("aroma_c_data2", 2); //시작분
+            Integer end_hour = sp_aroma_settings.getInt("aroma_c_data3", 3); //종료시
+            Integer end_minute = sp_aroma_settings.getInt("aroma_c_data4", 4); //종료분
+            Integer cycle_week = sp_aroma_settings.getInt("aroma_c_data5", 5); //분사 주기(요일)
+            String cycle_week_string = sp_aroma_settings.getString("aroma_c_data5_1", "월");
+            Integer cycle_time = sp_aroma_settings.getInt("aroma_c_data6", 6); //분사주기 (분)
+            Integer aroma_type = sp_aroma_settings.getInt("aroma_c_data7", 7); // 향기종료. 사용안함
+            Integer aroma_remain = sp_aroma_settings.getInt("aroma_c_data8", 8); // 잔여량
+            String aroma_c_name = sp_aroma_settings.getString("aroma_c_name", "설정값 없음");
+
+            RelativeLayout aroma_box3 = (RelativeLayout) findViewById(R.id.aroma_box3);
+            String resName1 = "@drawable/aroma_" + aroma_c_name;
+            int resID1 = getResources().getIdentifier(resName1, "drawable", "com.healingtime.healingtime");
+            aroma_box3.setBackgroundResource(resID1);
+
+            ImageView aroma_status_image1 = (ImageView) findViewById(R.id.aroma_status_image1);
+            String resName2 = "@drawable/aroma_" + aroma_c_name;
+            int resID2 = getResources().getIdentifier(resName2, "drawable", "com.healingtime.healingtime");
+            aroma_status_image1.setImageResource(resID2);
+
+            RelativeLayout aroma_therapy_setting_value_c = (RelativeLayout) findViewById(R.id.aroma_therapy_setting_value_c);  //설정값 표시 화면 다시 보이게 만들기
+            aroma_therapy_setting_value_c.setVisibility(View.VISIBLE);
+
+            TextView aroma_therapy_setting_c_time = (TextView) findViewById(R.id.aroma_therapy_setting_c_time);  //설
+            String start_ampm ="오전";
+            String end_ampm ="오전";
+            if(start_hour >= 13) { start_ampm = "오후"; start_hour -= 12;}
+            if(end_hour >= 13) { end_ampm = "오후"; end_hour -= 12;}
+            String set1 = start_ampm + " " + start_hour.toString() + ":" + start_minute.toString() + " ~ " + end_ampm + " " + end_hour.toString() + ":" + end_minute.toString();
+            aroma_therapy_setting_c_time.setText(set1);
+
+            TextView aroma_therapy_setting_c_aroma = (TextView) findViewById(R.id.aroma_therapy_setting_c_aroma);  //설
+            String set2 = "향기이름: Basil" ;
+            aroma_therapy_setting_c_aroma.setText(set2);
+
+            TextView aroma_therapy_setting_c_repeat = (TextView) findViewById(R.id.aroma_therapy_setting_c_repeat);  //설
+            String set3 = cycle_week_string + "   주기: " + cycle_time.toString() + "분";
+            aroma_therapy_setting_c_repeat.setText(set3);
+        }
+    }
+
+    private void ShowDialog()
+    {
+        LayoutInflater dialog = LayoutInflater.from(this);
+        final View dialogLayout = dialog.inflate(R.layout.aroma_name_setting, null);
+        final Dialog myDialog = new Dialog(this);
+
+        myDialog.setTitle("대화상자 제목이다");
+        myDialog.setContentView(dialogLayout);
+        myDialog.show();
+
+/*        Button btn_ok = (Button)dialogLayout.findViewById(R.id.btn_ok);
+        Button btn_cancel = (Button)dialogLayout.findViewById(R.id.btn_cancel);
+
+        btn_ok.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //OK 누르면 할거
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                myDialog.cancel();
+            }
+        });*/
     }
 }
